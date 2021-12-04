62cd80d34d003e2236a49b978488512cb1dd0d63
Hikari Hayashi <rev.hikari@gmail.com>
2021-12-04 10:38:23 +0800

    Support react devtools

--- src/ReactPixiRenderer.ts
+++ src/ReactPixiRenderer.ts
@@ -4,4 +4,10 @@ import { ReactPixiHostConfig } from "./ReactPixiHostConfig";
 
 const ReactPixiRenderer = Reconciler(ReactPixiHostConfig);
 
+ReactPixiRenderer.injectIntoDevTools({
+  rendererPackageName: "@neetly/react-pixi-renderer",
+  version: "2.0.0",
+  bundleType: process.env.NODE_ENV === "development" ? 1 : 0,
+});
+
 export { ReactPixiRenderer };

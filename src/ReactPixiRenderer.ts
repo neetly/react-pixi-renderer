4653e429a7d5b18c274645a0e0b26e01eb2da939
Hikari Hayashi <rev.hikari@gmail.com>
2022-03-05 16:47:48 +0800

    Fix bundleType

--- src/ReactPixiRenderer.ts
+++ src/ReactPixiRenderer.ts
@@ -7,7 +7,7 @@ const ReactPixiRenderer = Reconciler(ReactPixiHostConfig);
 ReactPixiRenderer.injectIntoDevTools({
   rendererPackageName: "@neetly/react-pixi-renderer",
   version: "2.1.0",
-  bundleType: process.env.NODE_ENV === "development" ? 1 : 0,
+  bundleType: process.env.NODE_ENV !== "production" ? 1 : 0,
 });
 
 export { ReactPixiRenderer };

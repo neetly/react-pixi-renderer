ca2717fe0e89770dc1fd1135027c066cc9301a03
Hikari Hayashi <rev.hikari@gmail.com>
2022-03-12 14:57:24 +0800

    Bump version

--- src/ReactPixiRenderer.ts
+++ src/ReactPixiRenderer.ts
@@ -6,7 +6,7 @@ const ReactPixiRenderer = Reconciler(ReactPixiHostConfig);
 
 ReactPixiRenderer.injectIntoDevTools({
   rendererPackageName: "@neetly/react-pixi-renderer",
-  version: "3.0.0",
+  version: "3.0.1",
   bundleType: process.env.NODE_ENV !== "production" ? 1 : 0,
 });
 
